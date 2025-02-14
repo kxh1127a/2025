f1e960bafef20957b122c7d05783a9228adc2138
kxh1127a <kxh1127a@gmail.com>
2025-02-14 15:44:41 +0900

    250214 daily

--- java-eclipse-workspace/chap01/src/sec03/exam01/Hello.java
+++ java-eclipse-workspace/chap01/src/sec03/exam01/Hello.java
@@ -3,8 +3,9 @@ package sec03.exam01;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello, java");
+		
+		System.out.println("Hello, Java!");
+
 	}
 
 }
